80a1acd2a98a2784018d6db954f4c554eb3eea23
Tim Fanselow <tim.fanselow@gmail.com>
2020-01-27 00:19:22 +1300

    add .inputrc for pg up/down history search

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 ln -s ${SCRIPT_DIR}/.tmux.conf ~/
 ln -s ${SCRIPT_DIR}/.vimrc ~/
+ln -s ${SCRIPT_DIR}/.inputrc ~/
+ln -s ${SCRIPT_DIR}/.gitconfig ~/
+
 echo ". ${SCRIPT_DIR}/.bashrc_additions" >> ~/.bashrc
